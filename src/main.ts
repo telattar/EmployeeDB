fc3d33404cc6a4c862b8232f83be2d80e7ca9bca
telattar <tarteelafattahibrahim@gmail.com>
2024-10-13 14:57:37 +0300

    mixpanel

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
+
+config();
+//setting up mixpanel
+const mixpanel = require('mixpanel');
+export const panel = mixpanel.init(process.env.MIXPANEL_PROJECT_TOKEN, { protocol: 'http' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
