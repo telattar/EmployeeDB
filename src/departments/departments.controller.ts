a12823df7cef76b3844131e98212aa84252ea335
telattar <tarteelafattahibrahim@gmail.com>
2024-10-13 12:27:25 +0300

    delete department

--- src/departments/departments.controller.ts
+++ src/departments/departments.controller.ts
@@ -1,4 +1,4 @@
-import { Body, ConflictException, Controller, Get, HttpCode, HttpException, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, ConflictException, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
 import { DepartmentsService } from './departments.service';
 import { supabase } from 'src/supabase';
 import { CreateDepartmentDto } from './dto/create-department.dto';
@@ -34,5 +34,10 @@ export class DepartmentsController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async updateDepartment(@Param('id') id: string, @Body() newDetails: UpdateDepartmentDto) {
     return await this.departmentsService.updateDepartment(id, newDetails);
+  }
+
+  @Delete(':id')
+  async deleteDepartment(@Param('id') id: string) {
+    return await this.departmentsService.deleteDepartment(id);
   }
 }
